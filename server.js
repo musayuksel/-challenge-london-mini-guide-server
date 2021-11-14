100bb0db6754c983f96b852e0e3fb9ae39fb6da6
musayuksel <musayuxel@gmail.com>
2021-11-14 00:15:08 +0000

    Add Level 500 all route with params

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const Stratford = require("./data/Stratford.json");
 
-app.get("/pharmacies", (request, response) => {
-  response.send(Stratford.pharmacies);
+app.get("/:city/:catog", (request, response) => {
+  const city = request.params.city;
+  const catog = request.params.catog;
+  const dataCity = require(`./data/${city}.json`);
+  if (catog in dataCity) {
+    response.send(dataCity[catog]);
+  } else {
+    response.sendStatus(404);
+  }
 });
-app.get("/colleges", (request, response) => {
-  response.send(Stratford.colleges);
-});
-app.get("/doctors", (request, response) => {
-  response.send(Stratford.doctors);
-});
-app.get("/hospitals	", (request, response) => {
-  response.send(Stratford.hospitals);
-});
+// app.get("/:city/colleges", (request, response) => {
+//   cit
+//   response.send(Stratford.colleges);
+// });
+// app.get("/doctors", (request, response) => {
+//   response.send(Stratford.doctors);
+// });
+// app.get("/hospitals	", (request, response) => {
+//   response.send(Stratford.hospitals);
+// });
 
 app.listen(PORT, () => {
   console.log("server is running on PORT :" + PORT);
